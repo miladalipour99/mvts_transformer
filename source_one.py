2acc146518b65198af4b6f0a4e491649202226ba
miladalipour99 <60439091+miladalipour99@users.noreply.github.com>
2022-01-13 23:20:41 -0800

    Create source_one.py

---
+++ source_one.py
@@ -0,0 +1,2 @@
+https://drive.google.com/file/d/11e5DvPu2a9PYj9FP2sHcHaAjtUF94ayS/view?usp=sharing :::::::ALZ_mat_train
+https://drive.google.com/file/d/1hexh7pz-fwlszyxtqukCAGSVyuqHR8HC/view?usp=sharing :::::::ALZ_mat
